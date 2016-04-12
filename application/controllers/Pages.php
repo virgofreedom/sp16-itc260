262af843cf352a1a1b6a60f8facd2e4d057d5316
virgofreedom <rattana.neak@gmail.com>
2016-04-12 16:28:19 -0700

    here we complete the ci tour tutorial

---
+++ application/controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php
+//Pages.php
+class Pages extends CI_Controller {
+
+        public function view($page = 'home')
+        {
+                if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+                {
+                        // Whoops, we don't have a page for that!
+                        show_404();
+                }
+
+                $data['title'] = ucfirst($page); // Capitalize the first letter
+                $data['page_id'] = "My Clever Page ID"; 
+                $this->load->view('templates/header', $data);
+                $this->load->view('pages/'.$page, $data);
+                $this->load->view('templates/footer', $data);
+        }
+}
